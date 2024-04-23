8aab3ae346d2becab08397b95d65031468329b8f
nazmul <nazmulislam.ni897@gmail.com>
2024-04-23 23:48:08 +0600

    remove getstaticprops

--- src/pages/[page].js
+++ src/pages/[page].js
@@ -1,34 +1,62 @@
 import productApi from "@/Context/ProductInfoProvider/Api/productApi";
 import { getProductByCategory } from "@/Context/utility";
 import PageContent from "@/layouts/PageContent";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export const getStaticPaths = async () => {
-  const keys = Object.keys(productApi);
-  const paths = keys.map((page) => ({ params: { page } }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticPaths = async () => {
+//   const keys = Object.keys(productApi);
+//   const paths = keys.map((page) => ({ params: { page } }));
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// };
 
-export const getStaticProps = async ({ params }) => {
-  const pathname = params.page;
-  const products = await getProductByCategory();
-  const content =
-    products.length > 0
-      ? products.filter((product) => product.url === pathname)
-      : [];
-  return {
-    props: {
-      content,
-      pathname,
-    },
-  };
-};
+// export const getStaticProps = async ({ params }) => {
+//   const pathname = params.page;
+//   const products = await getProductByCategory();
+//   const content =
+//     products.length > 0
+//       ? products.filter((product) => product.url === pathname)
+//       : [];
+//   return {
+//     props: {
+//       content,
+//       pathname,
+//     },
+//   };
+// };
 
-export default function Hellos({ content, pathname }) {
-  if (content.length > 0) {
-    return <PageContent products={content} pathname={pathname} />;
+export default function index() {
+  const router = useRouter();
+  const pathname = router.query.page;
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    try {
+      const data = await getProductByCategory();
+      const content = data.filter((product) => product.url === pathname);
+      setProducts(content);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
   }
-  return false;
+
+  useEffect(() => {
+    setLoading(true);
+    getProducts();
+  }, []);
+
+  if (loading) return <div>loading...</div>;
+
+  if (!loading && products.length === 0)
+    return <div>Something went wrong! please try again.</div>;
+
+  if (products.length > 0 && !loading) {
+    return <PageContent products={products} pathname={pathname} />;
+  }
 }
