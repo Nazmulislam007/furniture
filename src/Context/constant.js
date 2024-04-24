38aa2baf04103e0b439797a4d5fa4f257714a2cc
nazmul <nazmulislam.ni897@gmail.com>
2024-04-24 15:19:39 +0600

    change ip

--- src/Context/constant.js
+++ src/Context/constant.js
@@ -1,5 +1,5 @@
-export const apiUrl = "http://localhost:3000";
-// export const apiUrl = "http://167.99.182.9:3000";
+// export const apiUrl = "http://localhost:3000";
+export const apiUrl = "http://167.99.182.9:3000";
 
 export const PLEASE_LOGIN = "Please login!";
 export const TAX_PERCENT = 18;
