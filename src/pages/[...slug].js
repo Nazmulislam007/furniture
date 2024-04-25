90d10c0fae3e98bad22035af1dc51a0d4c0c0d5c
nazmul <nazmulislam.ni897@gmail.com>
2024-04-25 20:08:30 +0600

    Can visited check

--- src/pages/[...slug].js
+++ src/pages/[...slug].js
@@ -1,12 +1,15 @@
 import { apiUrl } from "@/Context/constant";
 import { getProductByCategory } from "@/Context/utility";
 import PageContent from "@/layouts/PageContent";
+import { Box, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function index() {
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
   const [pathname, setPathname] = useState("");
+  const [canVisitLink, setCanVisitLink] = useState(0);
   const router = useRouter();
 
   async function getPathName() {
@@ -15,27 +18,51 @@ export default function index() {
       const res = await fetch(url);
       const data = await res.json();
       setPathname(data[0]?.landingpage_id);
+      setCanVisitLink(data[0]?.goto);
       return data[0]?.landingpage_id;
     }
   }
 
   useEffect(() => {
+    setLoading(true);
     async function fetchData() {
-      const pathname = await getPathName();
-      const products = await getProductByCategory();
-      const content =
-        products.length > 0
-          ? products.filter((product) => product.url === pathname)
-          : [];
-      setProducts(content);
+      try {
+        const pathname = await getPathName();
+        const products = await getProductByCategory();
+        const content =
+          products.length > 0
+            ? products.filter((product) => product.url === pathname)
+            : [];
+        setProducts(content);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+      }
     }
     fetchData();
   }, [router]);
 
-  return (
-    products.length > 0 &&
-    pathname.length > 0 && (
-      <PageContent products={products} pathname={pathname} />
-    )
-  );
+  let content = null;
+
+  if (loading)
+    content = (
+      <Box>
+        <Typography>Loading...</Typography>
+      </Box>
+    );
+
+  if (!canVisitLink && !loading)
+    content = (
+      <Box>
+        <Typography fontSize="24px" textAlign="center">
+          This page is currently unavailable!
+        </Typography>
+      </Box>
+    );
+
+  if (!loading && canVisitLink && products.length > 0 && pathname.length > 0)
+    content = <PageContent products={products} pathname={pathname} />;
+
+  return content;
 }
