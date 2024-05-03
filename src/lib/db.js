066f3ebf7fb05dfcbe73f257f9666196110d3b50
nazmul <nazmulislam.ni897@gmail.com>
2024-05-03 11:38:18 +0600

    DB config

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,19 +1,19 @@
 import mysql from "mysql2/promise";
 
 export async function query({ query, values = [] }) {
-  const dbconnection = await mysql.createConnection({
-    host: "localhost",
-    database: "renocommerce",
-    user: "nazmul",
-    password: "112233"
-  });
-
   // const dbconnection = await mysql.createConnection({
-  // 	host: "167.99.182.9",
-  // 	database: "renocommerce",
-  // 	user: "renocomdb",
-  // 	password: "aprjun365",
+  //   host: "localhost",
+  //   database: "renocommerce",
+  //   user: "nazmul",
+  //   password: "112233"
   // });
+
+  const dbconnection = await mysql.createConnection({
+    host: "167.99.182.9",
+    database: "renocommerce",
+    user: "renocomdb",
+    password: "aprjun365"
+  });
 
   try {
     const [results] = await dbconnection.execute(query, values);
