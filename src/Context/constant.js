acb8da8d6f3cde61295658b3824949c8927bd3ee
nazmul <nazmulislam.ni897@gmail.com>
2024-05-26 10:56:17 +0600

    Change Url name

--- src/Context/constant.js
+++ src/Context/constant.js
@@ -1,5 +1,6 @@
 // export const apiUrl = "http://localhost:3000";
-export const apiUrl = "http://167.99.182.9:3000";
+// export const apiUrl = "http://167.99.182.9:3000";
+export const apiUrl = "http://renocommerce.com:3000/";
 
 export const PLEASE_LOGIN = "Please login!";
 export const TAX_PERCENT = 18;
