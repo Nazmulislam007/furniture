9ac87b88c070d7a5bcc673fda1a0df6a891016f8
nazmul <nazmulislam.ni897@gmail.com>
2024-06-12 17:44:08 +0600

    fix

--- src/lib/uploader.js
+++ src/lib/uploader.js
@@ -5,8 +5,6 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => cb(null, file.originalname)
 });
 
-
-
 const runMiddleware = (req, res, fn) =>
   new Promise((resolve, reject) => {
     fn(req, res, (result) => {
@@ -17,37 +15,11 @@ const runMiddleware = (req, res, fn) =>
     });
   });
 
-export async function uploadMiddleware(req,res){
+export async function uploadMiddleware(req, res) {
   try {
     const upload = multer({ storage });
     const uploadMiddleware = upload.single("file");
     await runMiddleware(req, res, uploadMiddleware);
-   
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-export async function query({ query, values = [] }) {
-  // const dbconnection = await mysql.createConnection({
-  //   host: "localhost",
-  //   database: "renocommerce",
-  //   user: "nazmul",
-  //   password: "112233"
-  // });
-
-  const dbconnection = await mysql.createConnection({
-    host: "167.99.182.9",
-    database: "renocommerce",
-    user: "renocomdb",
-    password: "aprjun365"
-  });
-
-  try {
-    const [results] = await dbconnection.execute(query, values);
-    dbconnection.end();
-    return results;
   } catch (error) {
     console.log(error);
   }
--- src/pages/api/uploader/upload.js
+++ src/pages/api/uploader/upload.js
@@ -1,16 +1,36 @@
-import { uploadMiddleware } from "@/lib/uploader";
+import multer from "multer";
+import { createRouter } from "next-connect";
 
-export default async function handler(req, res) {
-  try {
-    await uploadMiddleware(req,res)
-    res.status(200).json({ data: "File uploaded successfully" });
-  } catch (error) {
-    res.status(500).json({ error: `Upload failed: ${error.message}` });
+const router = createRouter();
+
+// Set up multer storage and file destination
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "./public/company-logo", // Set the destination for uploaded files
+    filename: (req, file, cb) => cb(null, file.originalname) // Use the original file name
+  })
+});
+
+// Create a nextConnect handler
+export default router.handler({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry, something went wrong! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
   }
-}
+});
+
+// Configure the API route to handle POST requests with file uploads
+router.use(upload.single("file"));
+
+// Define the handler for the route
+router.post((req, res) => {
+  res.status(200).json({ data: "File uploaded successfully" });
+});
 
 export const config = {
   api: {
-    bodyParser: false
+    bodyParser: false // Disallow body parsing, since we will handle it using multer
   }
 };
