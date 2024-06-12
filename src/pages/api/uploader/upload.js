fe0985269be09d1d62bae4f60720a146e858cc01
nazmul <nazmulislam.ni897@gmail.com>
2024-06-12 18:04:26 +0600

    fix

--- src/pages/api/uploader/upload.js
+++ src/pages/api/uploader/upload.js
@@ -1,16 +1,7 @@
-import { uploadMiddleware } from "@/lib/uploader";
-
 export default async function handler(req, res) {
   try {
-    await uploadMiddleware(req, res);
     res.status(200).json({ message: "file uploaded!" });
   } catch (error) {
     console.log(error);
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false // Disallow body parsing, since we will handle it using multer
-  }
-};
