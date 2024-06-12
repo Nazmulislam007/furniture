f45ca4dd4772b936b639aea5fa7b6fbdfd9f5d8e
nazmul <nazmulislam.ni897@gmail.com>
2024-06-12 17:03:29 +0600

    fix

--- src/lib/uploader.js
+++ src/lib/uploader.js
@@ -1,10 +1,54 @@
-import multer from "multer/index";
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: "./public/company-logo",
   filename: (req, file, cb) => cb(null, file.originalname)
 });
 
-const upload = multer({ storage });
 
-export default upload;
+
+const runMiddleware = (req, res, fn) =>
+  new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+
+export async function uploadMiddleware(req,res){
+  try {
+    const upload = multer({ storage });
+    const uploadMiddleware = upload.single("file");
+    await runMiddleware(req, res, uploadMiddleware);
+   
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export async function query({ query, values = [] }) {
+  // const dbconnection = await mysql.createConnection({
+  //   host: "localhost",
+  //   database: "renocommerce",
+  //   user: "nazmul",
+  //   password: "112233"
+  // });
+
+  const dbconnection = await mysql.createConnection({
+    host: "167.99.182.9",
+    database: "renocommerce",
+    user: "renocomdb",
+    password: "aprjun365"
+  });
+
+  try {
+    const [results] = await dbconnection.execute(query, values);
+    dbconnection.end();
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/pages/api/uploader/upload.js
+++ src/pages/api/uploader/upload.js
@@ -1,26 +1,13 @@
-import upload from "@/lib/uploader";
-
-const runMiddleware = (req, res, fn) =>
-  new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
+import { uploadMiddleware } from "@/lib/uploader";
 
 export default async function handler(req, res) {
   try {
-    const uploadMiddleware = upload.single("file");
-    await runMiddleware(req, res, uploadMiddleware);
-
+    await uploadMiddleware(req,res)
     res.status(200).json({ data: "File uploaded successfully" });
   } catch (error) {
     res.status(500).json({ error: `Upload failed: ${error.message}` });
   }
 }
-
 
 export const config = {
   api: {
